b16b7f8528af9a86ddc3e3d42abc0c0b898bd039
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2019-10-13 20:55:43 -0300

    Added UDP Client

--- udp_client/download_file.py
+++ udp_client/download_file.py
@@ -1,4 +1,97 @@
+import time
+import socket
+
+CHUNK_SIZE = 2048
+DELIMITER = ';'
+MAX_TIMEOUTS = 5
+
 def download_file(server_address, name, dst):
   # TODO: Implementar UDP download_file client
   print('UDP: download_file({}, {}, {})'.format(server_address, name, dst))
+  chunks = {}
+
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  client_socket.settimeout(2)
+
+  response = send_message('download' + DELIMITER + name, server_address, client_socket)
+  if response == "File not found":
+    print("The requested file {} does not exist".format(name))
+    return 0
+  file_size, offset = response.split(DELIMITER, 1)
+  print(response)
+
+  send_message('start transmission', server_address, client_socket)
+
+  recv_file(file_size, chunks, offset)
+
+  write_file(file_size, chunks, offset, dst)
+
+  response = end_download(server_address, client_socket)
+  print(response)
+
+  client_socket.close()
   pass
+
+def send_message(message, server_address, client_socket):
+  must_transfer = True
+  while must_transfer:
+    client_socket.sendto(message.encode(), server_address)
+    try:
+      response, addr = client_socket.recvfrom(CHUNK_SIZE)
+      return response.decode()
+    except socket.timeout:
+      must_transfer = True
+
+def recv_file(file_size, chunks, offset):
+  # Tengo que meter el chunk en el diccionario
+  # Actualizar el ultimo recibido en general
+  # Chequear que estamos recibiendo en orden los chunks
+  # SI no es asi enviamos el ack del ultimo recibido en orden
+  # Si estamos recibiendo en orden enviamos el ack del recibido y actualizasmos el ultimo recibido en orden
+  # TODO NTH timeout si me desenchufan el server
+  # TODO Garantia de entrega: timeout
+  received_chunks = (-1, -1) #ultimo recibido en orden, ultimo recibido en general
+  total_received_size = 0
+  timeouts = 0
+  while total_received_size < file_size: # while no es fin or timeout
+    try:
+      response, addr = client_socket.recvfrom(CHUNK_SIZE)
+      offset_number, chunk = response.decode().split(DELIMITER, 1)
+      if chunk == 'FIN':
+        return 
+      chunks[offset_number] = chunk
+      expected_offset_number = received_chunks[0] + offset
+      total_received_size += CHUNK_SIZE
+      timeouts = 0
+    except socket.timeout:
+      if total_received_size < file_size and timeouts < MAX_TIMEOUTS:
+        timeouts += 1
+      else:
+        return
+    if offset_number > received_chunks[1]:
+      received_chunks[1] = offset_number
+    if expected_offset_number != offset_number:
+      client_socket.sendto(expected_offset_number.encode(), server_address)
+    else:
+      received_chunks[0] = offset_number
+      client_socket.sendto(offset_number.encode(), server_address)
+
+def write_file(file_size, chunks, offset, dst):
+  with open(dst, 'w') as f:
+    for i in range(0, file_size, offset):
+      f.write(chunks[i])
+
+
+
+
+def end_download(server_address, client_socket):
+  message = 'end'
+  must_transfer = True
+  while must_transfer:
+    client_socket.sendto(message.encode(), server_address)
+    try:
+      response, addr = client_socket.recvfrom(CHUNK_SIZE)
+      if response.decode() == 'ok':
+        return response
+    except socket.timeout:
+      must_transfer = True
--- udp_server/start_server.py
+++ udp_server/start_server.py
@@ -1,6 +1,9 @@
 import socket
 
 CHUNK_SIZE = 2048
+SIZE = '1200'
+OFFSET = '60'
+DELIMITER = ';'
 
 def start_server(server_address, storage_dir):
   print('UDP: start_server({}, {})'.format(server_address, storage_dir))
@@ -9,6 +12,13 @@ def start_server(server_address, storage_dir):
   print("The server is ready to receive")
   while True:
     message, client_address = server_socket.recvfrom(CHUNK_SIZE)
-    response = message.decode().upper()
-    server_socket.sendto(response.encode(), client_address)
+    operation, file_info = message.decode().split(DELIMITER, 1)
+    if operation == 'download':
+      print(file_info) #TODO: buscar archivo, validaciones
+      response = SIZE + DELIMITER + OFFSET
+      server_socket.sendto(response.encode(), client_address)
+      message, client_address = server_socket.recvfrom(CHUNK_SIZE)
+      message = message.decode()
+      print(message)
+      server_socket.sendto('ok'.encode(), client_address)
   server_socket.close()
