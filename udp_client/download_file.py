49a1cff7902e333aea2bf7cbcae6d7416d7105f6
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-10-15 01:32:43 -0300

    First working version. Still need to check package loss handling.

--- udp_client/download_file.py
+++ udp_client/download_file.py
@@ -4,31 +4,39 @@ import socket
 CHUNK_SIZE = 2048
 DELIMITER = ';'
 MAX_TIMEOUTS = 5
+SOCKET_TIMEOUT = 2
 
 SUCCESS = 0
 ERROR = 1
 
 
 def download_file(server_address, name, dst):
-    # TODO: Implementar UDP download_file client
-    print('UDP: download_file({}, {}, {})'.format(server_address, name, dst))
     chunks = {}
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(2)
+    # TODO: Implementar UDP download_file client
+    print('UDP: download_file({}, {}, {})'.format(server_address, name, dst))
 
-    response = send_message('download' + DELIMITER +
-                            name, server_address, client_socket)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(SOCKET_TIMEOUT)
+
+    status, response = send_message('download' + DELIMITER + name, server_address, client_socket)
+
+    if status != SUCCESS:
+        return status
+
     if response == 'File not found':
         print('The requested file {} does not exist'.format(name))
-        return 0
+        return ERROR
+
     file_size, offset = response.split(DELIMITER, 1)
     print('Received file_size {} and offset {}.'.format(file_size, offset))
 
     file_size = int(file_size)
     offset = int(offset)
 
-    send_message('start transmission' + DELIMITER + name, server_address, client_socket)
+    message = 'start transmission' + DELIMITER + name
+    client_socket.sendto(message.encode(), server_address)
+    print('Start transmission sent. Ready to receive file...')
 
     status = recv_file(client_socket, server_address, file_size, chunks, offset)
 
@@ -41,17 +49,17 @@ def download_file(server_address, name, dst):
         #print(response)
 
     client_socket.close()
-    pass
 
 
 def send_message(message, server_address, client_socket):
-    while True:
+    for _ in range(MAX_TIMEOUTS):
         client_socket.sendto(message.encode(), server_address)
         try:
             response, addr = client_socket.recvfrom(CHUNK_SIZE)
-            return response.decode()
+            return (SUCCESS, response.decode())
         except socket.timeout:
             print('Socket timed out attempting to send message!')
+    return (ERROR, '')
 
 
 def recv_file(client_socket, server_address, file_size, chunks, offset):
@@ -63,37 +71,69 @@ def recv_file(client_socket, server_address, file_size, chunks, offset):
     # TODO NTH timeout si me desenchufan el server
     # TODO Garantia de entrega: timeout
     # ultimo recibido en orden, ultimo recibido en general
-    received_chunks = [-1, -1]
+    #received_chunks = [-1*offset, -1*offset]
     total_received_size = 0
     timeouts = 0
+    #offset_number = -1*offset
     offset_number = -1
-    expected_offset_number = -1
+    last_ordered_chunk = -1*offset
+
     while total_received_size < file_size:  # while no es fin or timeout
         try:
             print('Waiting to receive chunk...')
-            response, addr = client_socket.recvfrom(CHUNK_SIZE)
+
+            response, addr = client_socket.recvfrom(offset)
             offset_number, chunk = response.decode().split(DELIMITER, 1)
             if chunk == 'END':
                 print('Received END!')
                 return SUCCESS
-            print('Received offset_number {} and chunk {}'.format(offset_number, chunk))
-            chunks[offset_number] = chunk
-            expected_offset_number = received_chunks[0] + offset
-            total_received_size += CHUNK_SIZE
+
             timeouts = 0
+
+            print('Received offset_number {} and chunk {}'.format(offset_number, chunk))
+            offset_number = int(offset_number)
+
+            if offset_number in chunks: # ya tenemos este chunk
+                client_socket.sendto(str(offset_number).encode(), server_address)
+                continue
+
+            chunks[offset_number] = chunk
+            total_received_size += offset
+            print('total_received_size={}'.format(total_received_size))
+
+
+            if (offset_number == 0):
+                # Si offset_number es 0 entonces inicializo last_ordered_chunk = 0, 
+                # seguro es la primera vez que lo recibo porque ya verifique arriba
+                # que no esta en chunks.
+                last_ordered_chunk = 0
+            else:
+                # sino, si offset_number es el que estaba esperando (last_ordered_chunk + offset) 
+                # me fijo cual es el siguiente que falta en la secuencia
+                if (last_ordered_chunk >= 0 and (offset_number == last_ordered_chunk + offset)): 
+                    last_ordered_chunk = offset_number
+                    #arranco del siguiente chunk al que acabo de recibir:
+                    pos = offset_number + offset 
+                    # me fijo si llego a total_received_size, sino agarro la siguiente 'pos' que no este en chunks:
+                    while (pos < total_received_size and pos in chunks): 
+                        pos += offset #incremento de a offset
+                    last_ordered_chunk = pos - offset
+
+            print('last_ordered_chunk = {}'.format(last_ordered_chunk))
+
+            #print('Send ack for received chunk {}'.format(offset_number))
+            client_socket.sendto(str(last_ordered_chunk if (last_ordered_chunk > 0) else 0).encode(), server_address)
+
         except socket.timeout:
             print('Receive chunk timed out!')
             if total_received_size < file_size and timeouts < MAX_TIMEOUTS:
                 timeouts += 1
+                print('Send ack for last available chunk offset {}'.format(last_ordered_chunk))
+                client_socket.sendto(str(last_ordered_chunk if (last_ordered_chunk > 0) else 0).encode(), server_address)
             else:
                 return ERROR
-        if offset_number > received_chunks[1]:
-            received_chunks[1] = offset_number
-        if expected_offset_number != offset_number:
-            client_socket.sendto(str(expected_offset_number).encode(), server_address)
-        else:
-            received_chunks[0] = offset_number
-            client_socket.sendto(str(offset_number).encode(), server_address)
+    return SUCCESS
+
 
 
 def write_file(file_size, chunks, offset, dst):
@@ -102,14 +142,14 @@ def write_file(file_size, chunks, offset, dst):
             f.write(chunks[i])
 
 
-def end_download(server_address, client_socket):
-    message = 'end'
-    must_transfer = True
-    while must_transfer:
-        client_socket.sendto(message.encode(), server_address)
-        try:
-            response, addr = client_socket.recvfrom(CHUNK_SIZE)
-            if response.decode() == 'ok':
-                return response
-        except socket.timeout:
-            must_transfer = True
+#def end_download(server_address, client_socket):
+#    message = 'end'
+#    must_transfer = True
+#    while must_transfer:
+#        client_socket.sendto(message.encode(), server_address)
+#        try:
+#            response, addr = client_socket.recvfrom(CHUNK_SIZE)
+#            if response.decode() == 'ok':
+#                return response
+#        except socket.timeout:
+#            must_transfer = True
--- udp_server/start_server.py
+++ udp_server/start_server.py
@@ -1,78 +1,99 @@
 import os, socket
 
 CHUNK_SIZE = 2048
-SIZE = '1200'
 OFFSET = '60'
 DELIMITER = ';'
 WINDOW = 4
 MAX_TIMEOUTS_WAIT = 5
+
+SUCCESS = 0
+ERROR = 1
+
 
 def start_server(server_address, storage_dir):
     print('UDP: start_server({}, {})'.format(server_address, storage_dir))
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(server_address)
 
-    print('The server is ready to receive')
-    while True:
-        server_socket.settimeout(None)
-        message, client_address = server_socket.recvfrom(CHUNK_SIZE)
-        operation, file_info = message.decode().split(DELIMITER, 1)
-
-        if operation == 'download':
-            print('Requested info to download file {}'.format(file_info))
-            response = SIZE + DELIMITER + OFFSET
-
-            server_socket.sendto(response.encode(), client_address)
-
+    try:
+        print('The server is ready to receive.')
+        while True:
+            server_socket.settimeout(None)
+            print('Waiting for client to send message...')
             message, client_address = server_socket.recvfrom(CHUNK_SIZE)
 
-            file_name = message.decode()
+            print('Received message {}'.format(message))
+            operation, file_info = message.decode().split(DELIMITER, 1)
 
-            # Recibir el nombre del archivo y validar que existe
-            # Informar al cliente del resultado
-            if (os.path.isfile(file_name)):
-                print('Found file {}'.format(file_name))
-                server_socket.sendto('ok'.encode(), client_address)
+            if operation == 'download':
+                print('Requested info to download file {}'.format(file_info))
+
+                # Recibir el nombre del archivo y validar que existe
+                # Informar al cliente del resultado
+                if (os.path.isfile(file_info)):
+                    print('Found file {}'.format(file_info))
+                    file_size = os.path.getsize(file_info)
+                    response = str(file_size) + DELIMITER + OFFSET
+                    server_socket.sendto(response.encode(), client_address)
+                else:
+                    server_socket.sendto('File not found'.encode(), client_address)
+
+            elif operation == 'start transmission':
+                print('Starting file transmission')
+
+                server_socket.settimeout(2)
+                send_file(server_socket, client_address, file_info, os.path.getsize(file_info), int(OFFSET))
             else:
-                server_socket.sendto('File not found'.encode(), client_address)
-
-        elif operation == 'start transmission':
-            print('Starting file transmission')
-            
-            server_socket.settimeout(2)
-            send_file(server_socket, client_address, file_info)
-        else:
-            continue
-
-    server_socket.close()
+                continue
+        return SUCCESS
+    except:
+        return ERROR
+    finally:
+        server_socket.close()
 
 
-def send_file(server_socket, client_address, file_name):
+def send_file(server_socket, client_address, file_name, file_size, offset):
     with open(file_name) as f:
-        for i in range(0, file_size, CHUNK_SIZE * WINDOW):
+        for i in range(0, file_size, offset * WINDOW):
+            chunks = {}
             # mappeo a los offsets de cada chunk con lo que lei del archivo. e.g: { 0 : 'manuelita ', 4: 'se marcho ' }
-            chunks = { str(i) : f.read(CHUNK_SIZE) for j in range(WINDOW) if (i + j*CHUNK_SIZE) < file_size } 
-            print('Read {}'.format(chunks))
+            for j in range(WINDOW):
+                chunk_offset = i + j*offset
+                if chunk_offset < file_size:
+                    chunks[str(chunk_offset)] = f.read(offset)
+            print('Read {} from {}'.format(chunks, file_size))
             # Mando los chunks de a ventanas de WINDOW,
-            # o sea mando una cantidad WINDOW de chunks de tamanio CHUNK_SIZE
+            # o sea mando una cantidad WINDOW de chunks de tamanio offset
             # y no sigo hasta asegurarme de que se recibieron bien todos
-            send_chunks(server_socket, client_address, chunks)
+            status = send_chunks(server_socket, client_address, chunks)
+            if (status != SUCCESS):
+                return status
+    return SUCCESS
 
 
 def send_chunks(server_socket, client_address, chunks):
     must_transfer = True
-    # Mandamos todos los chunks de la lista
-    for offset, chunk in chunks:
-        client_socket.sendto(chunk.encode(), server_address)
-    while must_transfer:
+    i = 0
+    while must_transfer and i < MAX_TIMEOUTS_WAIT:
+        # Mandamos todos los chunks de la lista que no
+        # recibieron ack
+        for offset, chunk in chunks.items():
+            print('Send offset {} chunk {}'.format(offset, chunk))
+            server_socket.sendto((offset + DELIMITER + chunk).encode(), client_address)
         try:
-            response, addr = client_socket.recvfrom(CHUNK_SIZE)
-            ack = response.decode()
-            chunks.pop(ack, None) # Borrar el ack si esta en el dict
-            must_transfer = chunks.empty()
+            for _ in range(len(chunks)):
+                response, addr = server_socket.recvfrom(CHUNK_SIZE)
+                ack = response.decode()
+                print('Received ack {}'.format(ack))
+                if ack in chunks:
+                    chunks.pop(ack) # Borrar el ack si esta en el dict
+                must_transfer = len(chunks) > 0
+                print('chunks not acked {}'.format(chunks))
         except socket.timeout:
-            # Mandamos todos los no recibidos
-            for offset, chunk in chunks:
-                client_socket.sendto(chunk.encode(), server_address)
+            print('Timeout while waiting for ack!')
+            continue
+    if i >= MAX_TIMEOUTS_WAIT:
+        return ERROR
+    return SUCCESS
 
 
